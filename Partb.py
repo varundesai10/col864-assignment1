a809d0638311875732b5f5640370a777182c04ec
varundesai10 <ee1180511@iitd.ac.in>
2021-03-08 22:01:26 +0530

    changed partb.py

--- Partb.py
+++ Partb.py
@@ -16,15 +16,20 @@ def Sensor_model(Obs_E, X):
     x_pos, y_pos =X
 
     probE_given_X = 1
+
     for i, sensor in enumerate(sensors):
         x_sen, y_sen = sensor
         a = int(max(np.abs(x_pos-x_sen), np.abs(np.abs(y_pos-y_sen))))
+        
         if a < len(p_sensor) and Obs_E[i] == 1:
             probE_given_X = probE_given_X*p_sensor[a]
+        
         elif a < len(p_sensor) and Obs_E[i] == 0:
             probE_given_X = probE_given_X *(1 - p_sensor[a])
+        
         elif a >= len(p_sensor) and Obs_E[i] == 1:
             probE_given_X = probE_given_X*0
+        
         elif a >= len(p_sensor) and Obs_E[i] == 0:
             probE_given_X = probE_given_X*1
 
@@ -33,12 +38,19 @@ def Sensor_model(Obs_E, X):
 def Motion_model(curr_pos, next_position):
     x, y = curr_pos
     x_next, y_next = next_position
-    if x_next >=30 or y_next >=30:
+    
+    #invalid positions
+    if x_next >=30 or y_next >=30 or x_next <0 or y_next<0 or x>=30 or y>=30 or x<0 or y<0:
         return 0.0
+    
     a_x = x_next - x
     a_y = y_next - y
+    
+    #no diagonal movement
     if a_x !=0 and a_y != 0:
         return 0.0
+
+    #normal movements
     if a_x == 1:
         return 0.3
     elif a_x == -1:
@@ -47,21 +59,28 @@ def Motion_model(curr_pos, next_position):
         return 0.4
     elif a_y == -1:
         return 0.1
+    
+    #stuck on edge of box
+    elif(x == x_next and x == 29):
+        return 0.3
+    elif(x==x_next and x==0):
+        return 0.2
+    elif(y==y_next and y==0):
+        return 0.1
+    elif(y==y_next and y==29):
+        return 0.4
     else:
-        return 0.0
+        return 0
+
 
 def HMM_filter(Obs_E, Bel):
     Bel_t_1 = np.zeros(30,30)
     for i in range(30):
         for j in range(30):
-            if j-1 >=0:
-                Bel_t_1 += Bel[i, j-1] * Motion_model((i, j-1), (i,j))
-            if j+1 < 30:
-                Bel_t_1 += Bel[i, j+1] * Motion_model((i, j+1), (i,j))
-            if i-1 >=0:
-                Bel_t_1 += Bel[i-1, j] * Motion_model((i-1,j), (i,j))
-            if i+1 < 30:
-                Bel_t_1 += Bel[i+1, j] * Motion_model((i+1, j), (i, j))
+            Bel_t_1[i,j] += Bel[i, j-1] * Motion_model((i, j-1), (i,j))
+            Bel_t_1[i,j] += Bel[i, j+1] * Motion_model((i, j+1), (i,j))
+            Bel_t_1[i,j] += Bel[i-1, j] * Motion_model((i-1,j), (i,j))
+            Bel_t_1[i,j] += Bel[i+1, j] * Motion_model((i+1, j), (i, j))
     eta = 0
     for i in range(30):
         for j in range(30):
